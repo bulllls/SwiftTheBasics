7f7f1ea30d8168142bdd8f96d25e267945320c97
Ruslan <ruslanfilistovich@gmail.com>
2019-09-13 14:01:54 +0300

    while, repeat-while

---
+++ 13 while, repeat-while.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var timerOne = 5
+print("Couting down")
+
+while timerOne > 0 {
+  print(timerOne)
+  timerOne -= 1
+}
+
+print("Start!")
+
+var timerTwo = 5
+print("Couting down")
+
+repeat {
+  print(timerTwo)
+  timerTwo -= 1
+} while timerTwo > 0
+
+print("Start!")
